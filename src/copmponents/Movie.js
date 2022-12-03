b5123c62b702e7f73767ff7955569f5cb8a65ad6
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2022-12-03 19:32:09 +0500

    some changes

--- src/copmponents/Movie.js
+++ src/copmponents/Movie.js
@@ -2,17 +2,16 @@ export default function Movie(props) {
   const { Title, Year, imdbID, Type, Poster } = props;
 
   return (
-    <div key={imdbID} className="card movie">
-      <div className="card-image waves-effect waves-block waves-light">
-        <img className="activator" src={Poster} alt="movie" />
+    <div key={imdbID} className="card">
+      <div className="card-image">
+        <img src={Poster} alt="movie" />
       </div>
       <div className="card-content">
-        <span className="card-title activator grey-text text-darken-4">
-          {Title}
-        </span>
-        <p>
-          {Year} <span className="right">{Type}</span>
-        </p>
+        <span className="card-title">{Title}</span>
+      </div>
+      <div className="card-action">
+        <span>{Year}</span>
+        <span className="right">{Type}</span>
       </div>
     </div>
   );
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,17 +1,18 @@
 export default function Header() {
-  const hash = "#";
   return (
-    <nav>
-      <div className="nav-wrapper container">
-        <a href={hash} className="brand-logo">
-          React
-        </a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <a href={hash}>Movie</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="navbar-fixed">
+      <nav>
+        <div className="nav-wrapper">
+          <a href="!#" className="brand-logo">
+            React
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="!#">Movie</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
